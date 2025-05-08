32438fba093768e7d4febee21e42dbb946716266
Владимир <vova1210@yandex.ru>
2025-05-08 09:09:54 +0300

    Новый проект

---
+++ subscriptionsService/src/test/java/com/webriz/subscriptionsservice/SubscriptionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webriz.subscriptionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
